357f8d2b48806fee29168c608f1b97f043f6cb23
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-12-20 21:55:05 -0600

    feat: etcd service

--- nixos/configurations/pyxis/default.nix
+++ nixos/configurations/pyxis/default.nix
@@ -1,13 +1,39 @@
 {
   config,
+  inputs,
   lib,
+  self,
   ...
 }: {
   imports = [
+    inputs.nixos-vault-module.nixosModule
     ./hardware.nix
   ];
 
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+
+  detsys.vaultAgent.systemd.services."etcd" = {
+    enable = true;
+    secretFiles = {
+      defaultChangeAction = "restart";
+      files = {
+        pki = {
+          changeAction = "reload";
+          perms = "0440";
+          template = ''
+            {{ with pkiCert "cluster/pki/etcd/issue/member" "common_name=etcd-server" "ip_sans=100.100.135.61" }}
+            {{ .Key }}
+            {{ .CA }}
+            {{ .Cert }}
+            {{ .Key | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/key.pem" "etcd" "etcd" "0400" }}
+            {{ .CA | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/ca.pem" "etcd" "etcd" "0400" }}
+            {{ .Cert | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/cert.pem" "etcd" "etcd" "0400" }}
+            {{ end }}
+          '';
+        };
+      };
+    };
+  };
 
   # personal modules
   justinrubek = {
@@ -15,6 +41,10 @@
 
     vault = {
       enable = true;
+      reader = {
+        enable = true;
+        secret_file = "${self}/secrets/nodes/controller1.yaml";
+      };
 
       # retry_join = [
       #   "http://bunky:8200"
@@ -42,10 +72,17 @@
   services = {
     etcd = {
       enable = true;
-      initialCluster = ["bunky=http://100.96.238.57:2380" "ceylon=http://100.101.20.26:2380" "pyxis=http://100.100.135.61:2380"];
+      initialCluster = ["bunky=https://100.96.238.57:2380" "ceylon=https://100.101.20.26:2380" "pyxis=https://100.100.135.61:2380"];
       # initialClusterState = "existing";
-      listenClientUrls = ["http://100.100.135.61:2379" "http://127.0.0.1:2379"];
-      listenPeerUrls = ["http://100.100.135.61:2380"];
+      listenClientUrls = ["https://100.100.135.61:2379" "https://127.0.0.1:2379"];
+      listenPeerUrls = ["https://100.100.135.61:2380"];
+      certFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/cert.pem";
+      peerTrustedCaFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/ca.pem";
+      keyFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/key.pem";
+      peerKeyFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/key.pem";
+      peerClientCertAuth = true;
+      peerCertFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/cert.pem";
+      trustedCaFile = "${config.detsys.vaultAgent.secretFilesRoot}cert/ca.pem";
     };
     # rpc.statd fix
     nfs.server.enable = true;
