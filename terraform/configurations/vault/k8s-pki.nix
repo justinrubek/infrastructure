db83ec56a2735f3be7930c9e3dc8572febdd5588
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-12-20 21:45:06 -0600

    feat(terraform/vault): pki for kubernetes cluster

---
+++ terraform/configurations/vault/k8s-pki.nix
@@ -0,0 +1,68 @@
+{
+  resource = {
+    vault_policy = {
+      etc_member = {
+        name = "etcd-member";
+        policy = ''
+          path "cluster/pki/etcd/issue/member"
+          {
+            capabilities = ["create", "update"]
+          }
+        '';
+      };
+    };
+    vault_approle_auth_backend_role = {
+      "cluster-controller" = {
+        backend = ''''${vault_auth_backend.approle.path}'';
+        role_name = "cluster-controller";
+        token_policies = ["etcd-member"];
+        token_max_ttl = 1800;
+        token_ttl = 1200;
+      };
+    };
+
+    vault_mount = {
+      "pki-etcd" = {
+        path = "cluster/pki/etcd";
+        type = "pki";
+      };
+      "pki-k8s" = {
+        path = "cluster/pki/k8s";
+        type = "pki";
+      };
+    };
+
+    vault_pki_secret_backend_role = {
+      "etcd-member" = {
+        allow_any_name = true;
+        backend = ''''${vault_mount.pki-etcd.path}'';
+        name = "member";
+        ttl = "2592000";
+      };
+      "kubelet" = {
+        allow_bare_domains = true;
+        allow_subdomains = false;
+        allowed_domains = ["kubelet"];
+        backend = ''''${vault_mount.pki-k8s.path}'';
+        name = "member";
+        ttl = "2592000";
+      };
+    };
+    vault_pki_secret_backend_root_cert = {
+      "etcd" = {
+        backend = ''''${vault_mount.pki-etcd.path}'';
+        common_name = "cluster/pki/etcd";
+        key_type = "rsa";
+        ttl = 315360000;
+        type = "internal";
+      };
+      "k8s" = {
+        backend = ''''${vault_mount.pki-k8s.path}'';
+        common_name = "cluster/pki/k8s";
+        key_type = "rsa";
+        ttl = 315360000;
+        type = "internal";
+      };
+    };
+  };
+}
