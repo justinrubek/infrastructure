9e1b5bbe9f74269d75422d5a28368cecb20811ec
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-12-22 12:50:12 -0600

    feat(nixos/modules): kubernetes.apiserver

---
+++ nixos/modules/kubernetes/apiserver.nix
@@ -0,0 +1,145 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  top = config.services.justinrubek.kubernetes;
+  cfg = top.apiserver;
+
+  all_ips = lib.attrSets.attrValues (lib.attrsets.mapAttrs (name: value: value.ip) top.nodes);
+  internal_ip = "${top.nodes.${config.networking.hostName}.ip}";
+  systemd_unit = "kube-apiserver";
+  systemd_user = config.systemd.services.${systemd_unit}.serviceConfig.User;
+
+  mkCertSecret = {
+    subdir,
+    mount_path,
+    common_name,
+    ip_sans ? null,
+    systemd_user,
+    uri_sans ? null,
+  }: let
+    ip_sans = lib.strings.concatStringsSep "," ip_sans;
+    uri_sans = lib.strings.concatStringsSep "," ip_sans;
+  in {
+    changeAction = "reload";
+    perms = "0400";
+    template = ''
+      {{ with pkiCert "${mount_path}" "common_name=${common_name}" ${lib.mkIf (ip_sans != null) "ip_sans=${ip_sans}"} ${lib.mkIf (uri_sans != null) "uri_sans=${uri_sans}"} "ttl=420h" }}
+      {{ .CA }}
+      {{ .Cert }}
+      {{ .Key }}
+      {{ .CA | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/${subdir}/ca" "${systemd_user}" "${systemd_user}" "0400" }}
+      {{ .Cert | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/${subdir}/cert" "${systemd_user}" "${systemd_user}" "0400" }}
+      {{ .Key | writeToFile "${config.detsys.vaultAgent.secretFilesRoot}cert/${subdir}/key" "${systemd_user}" "${systemd_user}" "0400" }}
+      {{ end }}
+    '';
+  };
+
+  fileSecrets = {
+    etcd-pki = mkCertSecret {
+      inherit systemd_user;
+      subdir = "etcd";
+      mount_path = "cluster/pki/etcd/issue/member";
+      common_name = "etcd-server";
+      ip_sans = [
+        internal_ip
+        "127.0.0.1"
+      ];
+    };
+    apiserver-pki = mkCertSecret {
+      inherit systemd_user;
+      subdir = "kube-apiserver";
+      mount_path = "cluster/pki/k8s/issue/member";
+      common_name = "kube-apiserver";
+      ip_sans =
+        [
+          "10.96.0.1"
+          "127.0.0.1"
+        ]
+        ++ all_ips;
+      uri_sans = [
+        "kubernetes"
+        "kubernetes.default"
+        "kubernetes.default.svc"
+        "kubernetes.default.svc.cluster.local"
+      ];
+    };
+    service-account-pki = mkCertSecret {
+      inherit systemd_user;
+      subdir = "kube-service-account";
+      mount_path = "cluster/pki/k8s/issue/member";
+      common_name = "service-accounts";
+    };
+  };
+
+  certs = {
+    etcd = {
+      ca = "${config.detsys.vaultAgent.secretFilesRoot}cert/etcd/ca";
+      cert = "${config.detsys.vaultAgent.secretFilesRoot}cert/etcd/cert";
+      key = "${config.detsys.vaultAgent.secretFilesRoot}cert/etcd/key";
+    };
+    apiserver = {
+      ca = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-apiserver/ca";
+      cert = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-apiserver/cert";
+      key = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-apiserver/key";
+    };
+    service-account = {
+      ca = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-service-account/ca";
+      cert = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-service-account/cert";
+      key = "${config.detsys.vaultAgent.secretFilesRoot}cert/kube-service-account/key";
+    };
+  };
+in {
+  options = {
+    services.justinrubek.kubernetes.apiserver = {
+      enable = lib.mkEnableOption (lib.mdDoc "kubernetes api server");
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    detsys.vaultAgent.systemd.services.${systemd_unit} = {
+      enable = true;
+      secretFiles = {
+        defaultChangeAction = "restart";
+        files = {
+          inherit (fileSecrets) etcd-pki apiserver-pki service-account-pki;
+        };
+      };
+    };
+    services.kubernetes.apiserver = {
+      enable = true;
+      advertiseAddress = internal_ip;
+      allowPrivileged = true;
+      clientCaFile = "";
+      enableAdmissionPlugins = ["NodeRestriction" "ServiceAccount"];
+      etcd = {
+        servers = all_ips;
+        caFile = certs.etcd.ca;
+        certFile = certs.etcd.cert;
+        keyFile = certs.etcd.key;
+      };
+      extraOpts = ''
+        --apiserver-count=3 \
+        --audit-log-maxage=30 \
+        --audit-log-maxbackup=3 \
+        --audit-log-maxsize=100 \
+        --audit-log-path=/var/log/audit.log \
+        --enable-swagger-ui=true \
+        --enable-bootstrap-token-auth=true \
+        --event-ttl=1h \
+        --kubelet-https=true \
+        --service-node-port-range=30000-32767
+      '';
+      kubeletClientCaFile = certs.apiserver.ca;
+      kubeletClientCertFile = certs.apiserver.cert;
+      kubeletClientKeyFile = certs.apiserver.key;
+      runtimeConfig = "api/all";
+      serviceAccountKeyFile = certs.service-account.key;
+      serviceClusterIpRange = "10.96.0.0/24";
+      tlsCertFile = certs.apiserver.cert;
+      tlsKeyFile = certs.apiserver.key;
+      verbosity = 2;
+    };
+  };
+}
--- nixos/modules/kubernetes/default.nix
+++ nixos/modules/kubernetes/default.nix
@@ -5,9 +5,17 @@
 }: let
   cfg = config.services.justinrubek.kubernetes;
 in {
+  imports = [./apiserver.nix];
+
   options = {
     services.justinrubek.kubernetes = {
       enable = lib.mkEnableOption (lib.mdDoc "Kubernetes cluster components");
+      nodes = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule {
+          ip = lib.mkOption {type = lib.types.str;};
+        });
+        example = ''{ hostname.ip = "10.10.10.10"; }'';
+      };
     };
   };
 
